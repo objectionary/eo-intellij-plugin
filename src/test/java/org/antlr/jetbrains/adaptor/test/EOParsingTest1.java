b0eb5996c6d5eff8132cbd8795819a46923a7fc3
Stepa Strunkov <stepa.strunkov@gmail.com>
2022-06-01 15:27:42 +0300

    Add test with comments and fixed build.gradle

--- src/test/java/org/antlr/jetbrains/adaptor/test/EOParsingTest1.java
+++ src/test/java/org/antlr/jetbrains/adaptor/test/EOParsingTest1.java
@@ -3,9 +3,9 @@ package org.antlr.jetbrains.adaptor.test;
 import com.intellij.testFramework.ParsingTestCase;
 import org.antlr.jetbrains.eo.EOParserDefinition;
 
-public class EOParsingTest extends ParsingTestCase {
+public class EOParsingTest1 extends ParsingTestCase {
 
-    public EOParsingTest() {
+    public EOParsingTest1() {
         super("", "eo", new EOParserDefinition());
     }
 
@@ -18,7 +18,7 @@ public class EOParsingTest extends ParsingTestCase {
      */
     @Override
     protected String getTestDataPath() {
-        return "src/test/testData";
+        return "src/test/testData_1";
     }
 
     @Override
---
+++ src/test/java/org/antlr/jetbrains/adaptor/test/EOParsingTestСomments.java
@@ -0,0 +1,34 @@
+package org.antlr.jetbrains.adaptor.test;
+
+import com.intellij.testFramework.ParsingTestCase;
+import org.antlr.jetbrains.eo.EOParserDefinition;
+
+public class EOParsingTestСomments extends ParsingTestCase {
+
+    public EOParsingTestСomments() {
+        super("", "eo", new EOParserDefinition());
+    }
+
+    public void testParsingTestData() {
+        doTest(true);
+    }
+
+    /**
+     * @return path to test data file directory relative to root of this module.
+     */
+    @Override
+    protected String getTestDataPath() {
+        return "src/test/testData_2";
+    }
+
+    @Override
+    protected boolean skipSpaces() {
+        return false;
+    }
+
+    @Override
+    protected boolean includeRanges() {
+        return true;
+    }
+
+}
