59305b76d9514940cab30defdfd58665fc34f646
Stepa Strunkov <stepa.strunkov@gmail.com>
2022-06-02 18:26:08 +0300

    editing plugnin.xml and icons

--- src/main/java/org/antlr/jetbrains/eo/EOPluginController.java
+++ src/main/java/org/antlr/jetbrains/eo/EOPluginController.java
@@ -9,6 +9,16 @@ import com.intellij.openapi.project.Project;
 import com.intellij.openapi.util.SystemInfo;
 import org.jetbrains.annotations.NotNull;
 
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.InputStream;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 public class EOPluginController implements ProjectComponent {
 	public static final String PLUGIN_ID = "org.antlr.jetbrains.eo";
 	public static final Logger LOG = Logger.getInstance("EOPluginController");
@@ -38,8 +48,25 @@ public class EOPluginController implements ProjectComponent {
 		LOG.info("EO 0.0.1 Plugin version "+version+", Java version "+ SystemInfo.JAVA_VERSION);
 	}
 
+
 	@Override
-	public void initComponent() { }
+	public void initComponent() {
+		// System.out.println("init component\n\n");
+		/*try {
+			System.out.println("\ntry\n");
+			URL url = new URL("https://github.com/yasamprom/EO-intellij-plugin/blob/main/src/main/resources/org/antlr/jetbrains/eo/small_cactus2.png");
+			System.out.println(url.getPort());
+			Image image = ImageIO.read(url);
+			if (image == null) {
+				return;
+			}
+			System.out.println(image);
+			Icons.EO_ICON = new ImageIcon(image);
+		}
+		catch (java.io.IOException e){
+			System.out.println("\nfailed\n");
+		}*/
+	}
 
 	@Override
 	public void disposeComponent() { }
--- src/main/java/org/antlr/jetbrains/eo/Icons.java
+++ src/main/java/org/antlr/jetbrains/eo/Icons.java
@@ -1,9 +1,9 @@
 package org.antlr.jetbrains.eo;
 
 import com.intellij.openapi.util.IconLoader;
-
 import javax.swing.*;
 
+
 public class Icons {
-	public static final Icon EO_ICON = IconLoader.getIcon("/org/antlr/jetbrains/eo/eo_icon.png");
+	public static Icon EO_ICON = IconLoader.getIcon("/org/antlr/jetbrains/eo/small_cactus.png");
 }
