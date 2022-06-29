dd8ad590f013cce3f7e651c07f1be6c8cd5d9faa
Stepa Strunkov <stepa.strunkov@gmail.com>
2022-06-29 18:13:10 +0300

    added tokens to groups

--- src/main/java/org/eolang/jetbrains/EOColorSettingsPage.java
+++ src/main/java/org/eolang/jetbrains/EOColorSettingsPage.java
@@ -13,12 +13,13 @@ import org.jetbrains.annotations.Nullable;
 public class EOColorSettingsPage implements ColorSettingsPage {
   private static final AttributesDescriptor[] DESCRIPTORS =
     new AttributesDescriptor[] {
-      new AttributesDescriptor("Data", EOSyntaxHighlighter.CONST),
-      new AttributesDescriptor("Keywords", EOSyntaxHighlighter.COLON),
+      new AttributesDescriptor("Keywords", EOSyntaxHighlighter.KEYWORD),
       new AttributesDescriptor("Comments", EOSyntaxHighlighter.COMMENT),
       new AttributesDescriptor("Identifiers", EOSyntaxHighlighter.NAME),
       new AttributesDescriptor("Strings", EOSyntaxHighlighter.STRING),
-      new AttributesDescriptor("Metas", EOSyntaxHighlighter.META)
+      new AttributesDescriptor("Metas", EOSyntaxHighlighter.META),
+      new AttributesDescriptor("Constants", EOSyntaxHighlighter.NUMBERS),
+      new AttributesDescriptor("Braces", EOSyntaxHighlighter.BRACES)
     };
 
   @Nullable
@@ -42,13 +43,21 @@ public class EOColorSettingsPage implements ColorSettingsPage {
   @NotNull
   @Override
   public String getDemoText() {
-    return "+alias org.eolang.io.stdout\n"
-        + "# comment\n"
-        + "\n"
-        + "[] > app\n"
-        + "  stdout > @\n"
-        + "    \"Hello, world!\"\n"
-        + "123  #constant";
+    return "+alias org.eolang.io.stdout\n" +
+            "+alias org.eolang.txt.sprintf\n\n" +
+            "# is year leap?\n\n" +
+            "[args...] > main\n" +
+            "  [y] > leap\n" +
+            "    or. > @\n" +
+            "      and.\n" +
+            "        eq. (mod. y 4) 0\n" +
+            "        not. (eq. (mod. y 100) 0)\n" +
+            "      eq. (mod. y 400) 0\n" +
+            "  stdout > @\n" +
+            "    sprintf\n" +
+            "    \"%d is %sa leap year!\"\n" +
+            "    (args.get 0).as-int > year!\n" +
+            "  if. (leap year:y) \"\" \"not \"\n";
   }
 
   @NotNull
--- src/main/java/org/eolang/jetbrains/EOSyntaxHighlighter.java
+++ src/main/java/org/eolang/jetbrains/EOSyntaxHighlighter.java
@@ -14,72 +14,28 @@ import org.eolang.jetbrains.parser.EOLexer;
 import org.jetbrains.annotations.NotNull;
 
 public class EOSyntaxHighlighter extends SyntaxHighlighterBase {
-  public static final TextAttributesKey INT =
+  public static final TextAttributesKey NUMBERS =
       createTextAttributesKey("EO_INT", DefaultLanguageHighlighterColors.CONSTANT);
   public static final TextAttributesKey COMMENT =
       createTextAttributesKey("EO_COMMENT", DefaultLanguageHighlighterColors.LINE_COMMENT);
-  public static final TextAttributesKey BOOL =
-      createTextAttributesKey("EO_BOOL", DefaultLanguageHighlighterColors.CONSTANT);
   public static final TextAttributesKey META =
       createTextAttributesKey("EO_META", DefaultLanguageHighlighterColors.METADATA);
-  public static final TextAttributesKey STAR =
+  public static final TextAttributesKey KEYWORD =
       createTextAttributesKey("EO_STAR", DefaultLanguageHighlighterColors.KEYWORD);
-  public static final TextAttributesKey DOTS =
-      createTextAttributesKey("EO_DOTS", DefaultLanguageHighlighterColors.KEYWORD);
-  public static final TextAttributesKey CONST =
-      createTextAttributesKey("EO_CONST", DefaultLanguageHighlighterColors.KEYWORD);
-  public static final TextAttributesKey SLASH =
-      createTextAttributesKey("EO_SLASH", DefaultLanguageHighlighterColors.KEYWORD);
-  public static final TextAttributesKey COLON =
-      createTextAttributesKey("EO_COLON", DefaultLanguageHighlighterColors.KEYWORD);
-  public static final TextAttributesKey COPY =
-      createTextAttributesKey("EO_COPY", DefaultLanguageHighlighterColors.KEYWORD);
-  public static final TextAttributesKey ARROW =
-      createTextAttributesKey("EO_ARROW", DefaultLanguageHighlighterColors.KEYWORD);
-  public static final TextAttributesKey VERTEX =
-      createTextAttributesKey("EO_VERTEX", DefaultLanguageHighlighterColors.KEYWORD);
-  public static final TextAttributesKey SIGMA =
-      createTextAttributesKey("EO_SIGMA", DefaultLanguageHighlighterColors.KEYWORD);
-  public static final TextAttributesKey XI =
-      createTextAttributesKey("EO_XI", DefaultLanguageHighlighterColors.KEYWORD);
-  public static final TextAttributesKey PLUS =
-      createTextAttributesKey("EO_PLUS", DefaultLanguageHighlighterColors.KEYWORD);
-  public static final TextAttributesKey MINUS =
-      createTextAttributesKey("EO_MINUS", DefaultLanguageHighlighterColors.KEYWORD);
-  public static final TextAttributesKey QUESTION =
-      createTextAttributesKey("EO_QUESTION", DefaultLanguageHighlighterColors.KEYWORD);
   public static final TextAttributesKey SPACE =
       createTextAttributesKey("EO_SPACE", DefaultLanguageHighlighterColors.TEMPLATE_LANGUAGE_COLOR);
   public static final TextAttributesKey DOT =
       createTextAttributesKey("EO_DOT", DefaultLanguageHighlighterColors.DOT);
-  public static final TextAttributesKey LSQ =
+  public static final TextAttributesKey BRACES =
       createTextAttributesKey("EO_LSQ", DefaultLanguageHighlighterColors.BRACES);
-  public static final TextAttributesKey RSQ =
-      createTextAttributesKey("EO_RSQ", DefaultLanguageHighlighterColors.BRACES);
-  public static final TextAttributesKey LB =
-      createTextAttributesKey("EO_RB", DefaultLanguageHighlighterColors.BRACES);
-  public static final TextAttributesKey RB =
-      createTextAttributesKey("EO_RB", DefaultLanguageHighlighterColors.BRACES);
-  public static final TextAttributesKey AT =
-      createTextAttributesKey("EO_AT", DefaultLanguageHighlighterColors.KEYWORD);
-  public static final TextAttributesKey RHO =
-      createTextAttributesKey("EO_RHO", DefaultLanguageHighlighterColors.KEYWORD);
   public static final TextAttributesKey HASH =
       createTextAttributesKey("EO_HASH", DefaultLanguageHighlighterColors.LINE_COMMENT);
   public static final TextAttributesKey EOL =
       createTextAttributesKey("EO_EOL", DefaultLanguageHighlighterColors.TEMPLATE_LANGUAGE_COLOR);
-  public static final TextAttributesKey BYTES =
-      createTextAttributesKey("EO_BYTES", DefaultLanguageHighlighterColors.CONSTANT);
   public static final TextAttributesKey STRING =
       createTextAttributesKey("EO_STRING", DefaultLanguageHighlighterColors.STRING);
-  public static final TextAttributesKey FLOAT =
-      createTextAttributesKey("EO_FLOAT", DefaultLanguageHighlighterColors.CONSTANT);
-  public static final TextAttributesKey HEX =
-      createTextAttributesKey("EO_HEX", DefaultLanguageHighlighterColors.CONSTANT);
   public static final TextAttributesKey NAME =
       createTextAttributesKey("EO_NAME", DefaultLanguageHighlighterColors.IDENTIFIER);
-  public static final TextAttributesKey TEXT =
-      createTextAttributesKey("EO_TEXT", DefaultLanguageHighlighterColors.STRING);
   public static final TextAttributesKey BAD_CHARACTER =
       createTextAttributesKey("EO_BAD_CHARACTER", HighlighterColors.BAD_CHARACTER);
 
@@ -99,53 +55,37 @@ public class EOSyntaxHighlighter extends SyntaxHighlighterBase {
     int ttype = myType.getANTLRTokenType();
     TextAttributesKey attrKey;
     switch (ttype) {
-      case EOLexer.BOOL:
-        attrKey = BOOL;
-        break;
-      case EOLexer.COMMENT:
-        attrKey = COMMENT;
-        break;
-      case EOLexer.INT:
-        attrKey = INT;
-        break;
-      case EOLexer.STAR:
-        attrKey = STAR;
-        break;
-      case EOLexer.DOTS:
-        attrKey = DOTS;
-        break;
-      case EOLexer.CONST:
-        attrKey = CONST;
-        break;
       case EOLexer.SLASH:
-        attrKey = SLASH;
-        break;
+      case EOLexer.CONST:
+      case EOLexer.DOTS:
       case EOLexer.COLON:
-        attrKey = COLON;
-        break;
       case EOLexer.COPY:
-        attrKey = COPY;
-        break;
       case EOLexer.ARROW:
-        attrKey = ARROW;
-        break;
       case EOLexer.VERTEX:
-        attrKey = VERTEX;
-        break;
       case EOLexer.SIGMA:
-        attrKey = SIGMA;
-        break;
       case EOLexer.XI:
-        attrKey = XI;
-        break;
       case EOLexer.PLUS:
-        attrKey = PLUS;
-        break;
       case EOLexer.MINUS:
-        attrKey = MINUS;
-        break;
       case EOLexer.QUESTION:
-        attrKey = QUESTION;
+      case EOLexer.STAR:
+        attrKey = KEYWORD;
+        break;
+      case EOLexer.BOOL:
+      case EOLexer.INT:
+      case EOLexer.FLOAT:
+      case EOLexer.HEX:
+      case EOLexer.BYTES:
+        attrKey = NUMBERS;
+        break;
+      case EOLexer.LSQ:
+      case EOLexer.RSQ:
+      case EOLexer.LB:
+      case EOLexer.RB:
+        attrKey = BRACES;
+        break;
+      case EOLexer.STRING:
+      case EOLexer.TEXT:
+        attrKey = STRING;
         break;
       case EOLexer.SPACE:
         attrKey = SPACE;
@@ -153,23 +93,8 @@ public class EOSyntaxHighlighter extends SyntaxHighlighterBase {
       case EOLexer.DOT:
         attrKey = DOT;
         break;
-      case EOLexer.LSQ:
-        attrKey = LSQ;
-        break;
-      case EOLexer.RSQ:
-        attrKey = RSQ;
-        break;
-      case EOLexer.LB:
-        attrKey = LB;
-        break;
-      case EOLexer.RB:
-        attrKey = RB;
-        break;
-      case EOLexer.AT:
-        attrKey = AT;
-        break;
-      case EOLexer.RHO:
-        attrKey = RHO;
+      case EOLexer.COMMENT:
+        attrKey = COMMENT;
         break;
       case EOLexer.HASH:
         attrKey = HASH;
@@ -177,26 +102,14 @@ public class EOSyntaxHighlighter extends SyntaxHighlighterBase {
       case EOLexer.EOL:
         attrKey = EOL;
         break;
-      case EOLexer.BYTES:
-        attrKey = BYTES;
-        break;
-      case EOLexer.STRING:
-        attrKey = STRING;
-        break;
-      case EOLexer.FLOAT:
-        attrKey = FLOAT;
-        break;
-      case EOLexer.HEX:
-        attrKey = HEX;
-        break;
       case EOLexer.NAME:
         attrKey = NAME;
         break;
-      case EOLexer.TEXT:
-        attrKey = TEXT;
-        break;
       case EOLexer.META:
         attrKey = META;
+        break;
+      case EOLexer.BAD_CHARACTER:
+        attrKey = BAD_CHARACTER;
         break;
       default:
         return EMPTY_KEYS;
