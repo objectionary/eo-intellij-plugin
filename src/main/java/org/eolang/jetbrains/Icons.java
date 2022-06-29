7c2bdea480938b3386c31773084f0a2c15eba997
Yegor Bugayenko <yegor256@gmail.com>
2022-06-29 13:19:44 +0300

    #colors: new icon

--- src/main/java/org/eolang/jetbrains/Icons.java
+++ src/main/java/org/eolang/jetbrains/Icons.java
@@ -1,8 +1,8 @@
 package org.eolang.jetbrains;
 
 import com.intellij.openapi.util.IconLoader;
-import javax.swing.*;
+import javax.swing.Icon;
 
 public class Icons {
-  public static Icon EO_ICON = IconLoader.getIcon("/org/eolang/jetbrains/cactus-16.png");
+  public static Icon EO_ICON = IconLoader.getIcon("/org/eolang/jetbrains/cactus-64.png");
 }
