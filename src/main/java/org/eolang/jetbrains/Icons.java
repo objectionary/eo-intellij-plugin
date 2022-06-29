160c065faa18874b6bcec49292fc6e4d534a39fb
Stepa Strunkov <stepa.strunkov@gmail.com>
2022-06-29 17:15:45 +0300

    replace png to svg

--- src/main/java/org/eolang/jetbrains/Icons.java
+++ src/main/java/org/eolang/jetbrains/Icons.java
@@ -4,5 +4,5 @@ import com.intellij.openapi.util.IconLoader;
 import javax.swing.Icon;
 
 public class Icons {
-  public static Icon EO_ICON = IconLoader.getIcon("/org/eolang/jetbrains/cactus-64.png");
+  public static Icon EO_ICON = IconLoader.getIcon("/org/eolang/jetbrains/cactus-16svg.svg");
 }
