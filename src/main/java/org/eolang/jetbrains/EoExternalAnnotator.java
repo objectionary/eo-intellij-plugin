d923052aab02f52175bfcd69946b0536ca6ced45
xzescha <50021439+xzescha@users.noreply.github.com>
2025-03-25 16:17:37 +0300

    Fixed handwritten files pt.2

--- src/main/java/org/eolang/jetbrains/EoExternalAnnotator.java
+++ src/main/java/org/eolang/jetbrains/EoExternalAnnotator.java
@@ -1,44 +1,24 @@
 /*
  * The MIT License (MIT)
  *
- *
- *
  * Copyright (c) 2021-2022 Stepan Strunkov
  *
- *
- *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
- *
  * of this software and associated documentation files (the "Software"), to deal
- *
  * in the Software without restriction, including without limitation the rights
- *
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- *
  * copies of the Software, and to permit persons to whom the Software is
- *
  * furnished to do so, subject to the following conditions:
  *
- *
- *
  * The above copyright notice and this permission notice shall be included
- *
  * in all copies or substantial portions of the Software.
  *
- *
- *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- *
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- *
  * FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO EVENT SHALL THE
- *
  * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- *
  * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- *
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- *
  * SOFTWARE.
  */
 
@@ -54,12 +34,15 @@ import java.util.List;
 import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
 
+//@checkstyle JavadocTagsCheck (9 lines)
+
 /**
  * An external annotator is an object that analyzes code in a document and annotates the PSI
  * elements with errors or warnings. Because such analysis can be expensive, we don't want it in the
  * GUI event loop. Jetbrains provides this external annotator mechanism to run these analyzers out
  * of band.
  */
+@SuppressWarnings("PMD.SystemPrintln")
 public class EoExternalAnnotator
     extends ExternalAnnotator<PsiFile, List<EoExternalAnnotator.Issue>> {
 
@@ -84,8 +67,8 @@ public class EoExternalAnnotator
     }
 
     // Called 3rd to actually annotate the editor window.
+
     @Override
-    @SuppressWarnings("PMD:SystemPrintln")
     public final void apply(@NotNull final PsiFile file, final @NotNull List<Issue> issues,
         @NotNull final AnnotationHolder holder) {
         for (final Issue issue : issues) {
--- src/test/java/org/eolang/jetbrains/EoParsingTest.java
+++ src/test/java/org/eolang/jetbrains/EoParsingTest.java
@@ -1,44 +1,24 @@
 /*
  * The MIT License (MIT)
  *
- *
- *
  * Copyright (c) 2021-2022 Stepan Strunkov
  *
- *
- *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
- *
  * of this software and associated documentation files (the "Software"), to deal
- *
  * in the Software without restriction, including without limitation the rights
- *
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- *
  * copies of the Software, and to permit persons to whom the Software is
- *
  * furnished to do so, subject to the following conditions:
  *
- *
- *
  * The above copyright notice and this permission notice shall be included
- *
  * in all copies or substantial portions of the Software.
  *
- *
- *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- *
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- *
  * FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO EVENT SHALL THE
- *
  * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- *
  * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- *
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- *
  * SOFTWARE.
  */
 
@@ -68,24 +48,26 @@ public final class EoParsingTest extends ParsingTestCase {
      * @todo #30:30min Enable the tests. Since new grammar was added to eolang
      *  need to update antlr4 grammar, remake and refactor parsing tests. Don't
      *  forget to get codecov coverage level up before resolving this task.
-     */
+      */
     @Test
     public void testParsingTestData() {
         doTest(false);
     }
 
+    // @checkstyle ProtectedMethodInFinalClassCheck (12 lines)
     @Override
-    protected final String getTestDataPath() {
+    protected String getTestDataPath() {
         return "src/test/testData_1";
     }
 
-    // @checkstyle DesignForExtensionCheck (3 lines)
+    // @checkstyle DesignForExtensionCheck (5 lines)
     @Override
     protected boolean skipSpaces() {
         return false;
     }
 
-    // @checkstyle DesignForExtensionCheck (3 lines)
+    // @checkstyle DesignForExtensionCheck (5 lines)
+    // @checkstyle ProtectedMethodInFinalClassCheck (5 lines)
     @Override
     protected boolean includeRanges() {
         return true;
