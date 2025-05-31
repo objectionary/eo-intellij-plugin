fa5028933722e67c43273287a32e70d782ba129f
xzescha <50021439+xzescha@users.noreply.github.com>
2025-06-01 01:46:43 +0300

    Added new test for performUnsafe in EoCompileAction

--- src/test/java/org/eolang/jetbrains/EoCompileActionTest.java
+++ src/test/java/org/eolang/jetbrains/EoCompileActionTest.java
@@ -8,9 +8,16 @@
 
 package org.eolang.jetbrains;
 
+import com.intellij.openapi.actionSystem.ActionManager;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.DataContext;
+import com.intellij.openapi.actionSystem.Presentation;
+import java.lang.reflect.Method;
 import java.util.List;
 import org.junit.Assert;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 
 /**
  * Unit tests for EoCompileAction helper methods.
@@ -33,6 +40,47 @@ public final class EoCompileActionTest {
             "Goals should match expected sequence",
             expected,
             goals.toArray(new String[0])
+        );
+    }
+
+    @SuppressWarnings("PMD.AvoidAccessibilityAlteration")
+    @Test
+    void performUnsafeThrowsIfNoProject() throws Exception {
+        final DataContext emptyctx = dataId -> null;
+        final Presentation presentation = new Presentation();
+        final ActionManager actionman = Mockito.mock(ActionManager.class);
+        final String place = "UnitTest";
+        final AnActionEvent event = new AnActionEvent(
+            null,
+            emptyctx,
+            place,
+            presentation,
+            actionman,
+            0
+        );
+        final Method perform = EoCompileAction.class.getDeclaredMethod(
+            "performUnsafe",
+            AnActionEvent.class
+        );
+        perform.setAccessible(true);
+        final Exception exception = Assertions.assertThrows(
+            Exception.class,
+            () -> perform.invoke(
+                null,
+                event
+            )
+        );
+        Throwable cause = exception;
+        if (exception.getCause() != null) {
+            cause = exception.getCause();
+        }
+        Assertions.assertInstanceOf(
+            IllegalStateException.class,
+            cause
+        );
+        Assertions.assertEquals(
+            "Select IDEA project",
+            cause.getMessage()
         );
     }
 }
