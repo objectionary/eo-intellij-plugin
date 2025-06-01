d4d604804c4c53cc06dcb370a7bd1e6b9d7b92e9
xzescha <50021439+xzescha@users.noreply.github.com>
2025-06-01 03:37:38 +0300

    Added new test for performUnsafe in EoCompileAction, extended buildGoals test

--- src/test/java/org/eolang/jetbrains/EoCompileActionTest.java
+++ src/test/java/org/eolang/jetbrains/EoCompileActionTest.java
@@ -8,15 +8,21 @@
 
 package org.eolang.jetbrains;
 
+import com.intellij.notification.Notification;
+import com.intellij.notification.NotificationGroup;
+import com.intellij.notification.NotificationGroupManager;
+import com.intellij.notification.NotificationType;
 import com.intellij.openapi.actionSystem.ActionManager;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.DataContext;
 import com.intellij.openapi.actionSystem.Presentation;
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.List;
 import org.junit.Assert;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.mockito.MockedStatic;
 import org.mockito.Mockito;
 
 /**
@@ -24,13 +30,14 @@ import org.mockito.Mockito;
  * Test class @EoCompileActionTest.
  * @since 0.0.1
  */
-@SuppressWarnings("PMD.JUnit5TestShouldBePackagePrivate")
+@SuppressWarnings({"PMD.JUnit5TestShouldBePackagePrivate", "PMD.AvoidAccessibilityAlteration"})
 public final class EoCompileActionTest {
 
     @SuppressWarnings("PMD.ProhibitPlainJunitAssertionsRule")
     @Test
     void buildGoals() {
-        final List<String> goals = EoCompileAction.buildGoals("0.56.0");
+        final EoCompileAction action = new EoCompileAction();
+        final List<String> goals = action.buildGoals("0.56.0");
         final String[] expected = {
             "org.eolang:eo-maven-plugin:0.56.0:register",
             "org.eolang:eo-maven-plugin:0.56.0:transpile",
@@ -43,7 +50,6 @@ public final class EoCompileActionTest {
         );
     }
 
-    @SuppressWarnings("PMD.AvoidAccessibilityAlteration")
     @Test
     void performUnsafeThrowsIfNoProject() throws Exception {
         final DataContext emptyctx = dataId -> null;
@@ -82,5 +88,147 @@ public final class EoCompileActionTest {
             "Select IDEA project",
             cause.getMessage()
         );
+    }
+
+    @Test
+    void runBuildThrowsIfNoProject() throws Exception {
+        final DataContext emptyctx = dataId -> null;
+        final Presentation presentation = new Presentation();
+        final ActionManager actionman = Mockito.mock(ActionManager.class);
+        final String place = "UnitTest";
+        final AnActionEvent event = new AnActionEvent(
+            null,
+            emptyctx,
+            place,
+            presentation,
+            actionman,
+            0
+        );
+        final Method runbuild = EoCompileAction.class.getDeclaredMethod(
+            "runBuild",
+            AnActionEvent.class
+        );
+        runbuild.setAccessible(
+            true
+        );
+        final Exception exception = Assertions.assertThrows(
+            Exception.class,
+            () -> runbuild.invoke(
+                null,
+                event
+            )
+        );
+        Throwable cause = exception;
+        if (exception.getCause() != null) {
+            cause = exception.getCause();
+        }
+        Assertions.assertTrue(cause instanceof IllegalStateException);
+        Assertions.assertEquals(
+            "No IDEA project selected",
+            cause.getMessage()
+        );
+    }
+
+    @Test
+    void notifyCannotCompileDoesNotThrow() throws Exception {
+        final Method notify = EoCompileAction.class.getDeclaredMethod(
+            "notifyCannotCompile",
+            String.class
+        );
+        notify.setAccessible(
+            true
+        );
+        final InvocationTargetException exception = Assertions.assertThrows(
+            InvocationTargetException.class,
+            () -> notify.invoke(
+                null,
+                "some error"
+            ),
+            "Expecting NPE, because in default unit-env NotificationGroupManager.getInstance() throws null"
+        );
+        Assertions.assertTrue(
+            exception.getCause() instanceof NullPointerException,
+            "Expecting, that getCause() in InvocationTargetException will be NullPointerException"
+        );
+    }
+
+    @Test
+    void notifyCannotCompileInvokesNotificationCorrectly() throws Exception {
+        final Notification mocknotify = Mockito.mock(
+            Notification.class
+        );
+        final NotificationGroup mockgroup = Mockito.mock(
+            NotificationGroup.class
+        );
+        Mockito.when(
+            mockgroup.createNotification(
+                Mockito.eq(
+                    "Cannot compile EO"
+                ),
+                Mockito.eq(
+                    "Test-reason"
+                ),
+                Mockito.eq(
+                    NotificationType.WARNING
+                )
+            )
+        ).thenReturn(
+            mocknotify
+        );
+        final NotificationGroupManager mockmanag = Mockito.mock(
+            NotificationGroupManager.class
+        );
+        Mockito.when(
+            mockmanag.getNotificationGroup(
+                "System Messages"
+            )
+        ).thenReturn(
+            mockgroup
+        );
+        try (MockedStatic<NotificationGroupManager> mockedStatic = Mockito.mockStatic(
+            NotificationGroupManager.class
+        )
+        ) {
+            mockedStatic.when(
+                NotificationGroupManager::getInstance
+            ).thenReturn(
+                mockmanag
+            );
+            final Method notifymeth = EoCompileAction.class.getDeclaredMethod(
+                "notifyCannotCompile",
+                String.class
+            );
+            notifymeth.setAccessible(
+                true
+            );
+            final InvocationTargetException thrown = Assertions.assertThrows(
+                InvocationTargetException.class,
+                () -> notifymeth.invoke(
+                    null,
+                    "Test-reason"
+                ),
+                "Expect that notification.notifyAll() will throw IllegalMonitorStateException"
+            );
+            Assertions.assertTrue(
+                thrown.getCause() instanceof IllegalMonitorStateException,
+                "Expect getCause() to be equal IllegalMonitorStateException"
+            );
+            Mockito.verify(
+                mockmanag, Mockito.times(
+                    1
+                )
+            ).getNotificationGroup(
+                "System Messages"
+            );
+            Mockito.verify(
+                mockgroup, Mockito.times(
+                    1
+                )
+            ).createNotification(
+                "Cannot compile EO",
+                "Test-reason",
+                NotificationType.WARNING
+            );
+        }
     }
 }
